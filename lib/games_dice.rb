997b067510540faa7e0b52e527639b3fb4afd7cc
Neil Slater <neil@aframe.com>
2013-04-09 20:33:54 +0100

    Minor changes to documentation

--- lib/games_dice.rb
+++ lib/games_dice.rb
@@ -4,6 +4,7 @@ require "games_dice/die_result"
 require "games_dice/reroll_rule"
 require "games_dice/map_rule"
 require "games_dice/complex_die"
+require "games_dice/bunch"
 
 module GamesDice
   # TODO: Factory methods for various dice schemes
