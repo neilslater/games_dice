09e46da8f85b990c4b8bb692db9336c63c11f1e3
Neil Slater <neil@aframe.com>
2013-04-09 19:23:53 +0100

    Add MapRule

--- lib/games_dice.rb
+++ lib/games_dice.rb
@@ -2,6 +2,7 @@ require "games_dice/version"
 require "games_dice/die"
 require "games_dice/die_result"
 require "games_dice/reroll_rule"
+require "games_dice/map_rule"
 
 module GamesDice
   # TODO: Factory methods for various dice schemes
