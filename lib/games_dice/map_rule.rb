c6a77afc43f5d4006f94bd3d5d971f56f6e0733b
Neil Slater <neil@aframe.com>
2013-06-10 21:34:58 +0100

    Document GamesDice::MapRule

--- lib/games_dice/map_rule.rb
+++ lib/games_dice/map_rule.rb
@@ -19,8 +19,14 @@
 
 class GamesDice::MapRule
 
-  # trigger_op, trigger_value, mapped_value and mapped_name set the attributes of the same name
-  #  rule = RPGMapRule.new( 6, :<=, 1, 'Success' ) # score 1 for a result of 6 or more
+  # Creates new instance of GamesDice::MapRule. The rule will be assessed as
+  #   trigger_value.send( trigger_op, x )
+  # where x is the Integer value shown on a die.
+  # @param [Integer,Range<Integer>,Object] trigger_value Any object is allowed, but typically an Integer
+  # @param [Symbol] trigger_op A method of trigger_value that takes an Integer param and returns Boolean
+  # @param [Integer] mapped_value The value to use in place of the trigger value
+  # @param [String] mapped_name Name of mapped value, for use in descriptions
+  # @return [GamesDice::MapRule]
   def initialize trigger_value, trigger_op, mapped_value=0, mapped_name=''
 
     if ! trigger_value.respond_to?( trigger_op )
@@ -34,23 +40,25 @@ class GamesDice::MapRule
     @mapped_name = mapped_name.to_s
   end
 
-  # an Integer value or Range that will be mapped to a single value. #trigger_op is called against it
-  attr_reader :trigger_value
-
-  # a valid symbol for a method, which will be called against #trigger_value with the current
-  # die result as a param. If the operator returns true for a specific die result, then the
-  # mapped_value will be used in its stead. If the operator returns nil or false, the map is not
-  # triggered. All other values will be returned as the result of the map (allowing you to
-  # specify any method that takes an integer as input and returns something else as the end result)
+  # Trigger operation. How the rule is assessed against #trigger_value.
+  # @return [Symbol] Method name to be sent to #trigger_value
   attr_reader :trigger_op
 
-  # an integer value
+  # Trigger value. An object that will use #trigger_op to assess a die result for a reroll.
+  # @return [Integer,Range,Object] Object that receives (#trigger_op, die_result)
+  attr_reader :trigger_value
+
+  # Mapped value.
+  # @return [Integer]
   attr_reader :mapped_value
 
-  # a string description of the mapping, e.g. 'S' for a success
+  # Name for mapped value.
+  # @return [String]
   attr_reader :mapped_name
 
-  # runs the rule against test_value, returning either a new value, or nil if the rule does not apply
+  # Assesses the rule against a die result value.
+  # @param [Integer] test_value Value that is result of rolling a single die.
+  # @return [Integer,nil] Replacement value, or nil if this rule doesn't apply
   def map_from test_value
     op_result = @trigger_value.send( @trigger_op, test_value )
     return nil unless op_result
