8a378bda1c8f68792457a71bf6180ed1252c203b
Neil Slater <neil@aframe.com>
2013-06-27 22:06:23 +0100

    NewProbabilities.add_distributions_mult

--- ext/games_dice/probabilities.c
+++ ext/games_dice/probabilities.c
@@ -1,8 +1,27 @@
 // ext/games_dice/probabilities.c
 
+#include <stdio.h>
 #include "probabilities.h"
 
 VALUE NewProbabilities = Qnil;
+
+static inline int max( int *a, int n ) {
+  int m = -1000000000;
+  int i;
+  for ( i=0; i < n; i++ ) {
+    m = a[i] > m ? a[i] : m;
+  }
+  return m;
+}
+
+static inline int min( int *a, int n ) {
+  int m = 1000000000;
+  int i;
+  for ( i=0; i < n; i++ ) {
+    m = a[i] < m ? a[i] : m;
+  }
+  return m;
+}
 
 static ProbabilityList *create_probability_list() {
   ProbabilityList *pl;
@@ -27,6 +46,7 @@ static double *alloc_probs( ProbabilityList *pl, int slots ) {
     rb_raise(rb_eArgError, "Bad number of probability slots");
   }
   pl->slots = slots;
+  // TODO: Should this be a NALLOC?
   double *pr = malloc( slots * sizeof(double));
   if (pr == NULL) {
     rb_raise(rb_eRuntimeError, "Could not allocate memory for NewProbabilities");
@@ -35,7 +55,7 @@ static double *alloc_probs( ProbabilityList *pl, int slots ) {
   return pr;
 }
 
-static void init_probs_iv( ProbabilityList *pl, int slots, double iv ) {
+static double *alloc_probs_iv( ProbabilityList *pl, int slots, double iv ) {
   if ( iv < 0.0 || iv > 1.0 ) {
     rb_raise(rb_eArgError, "Bad single probability value");
   }
@@ -44,15 +64,26 @@ static void init_probs_iv( ProbabilityList *pl, int slots, double iv ) {
   for(i=0; i<slots; i++) {
     pr[i] = iv;
   }
-  return;
+  return pr;
 }
 
-static VALUE pl_alloc(VALUE klass) {
-  ProbabilityList *pl;
+static inline int pl_min( ProbabilityList *pl ) {
+  return pl->offset;
+}
+
+static inline int pl_max( ProbabilityList *pl ) {
+  return pl->offset + pl->slots - 1;
+}
+
+static inline VALUE pl_as_ruby_class( ProbabilityList *pl, VALUE klass ) {
   VALUE obj;
   pl = create_probability_list();
   obj = Data_Wrap_Struct( klass, 0, destroy_probability_list, pl );
   return obj;
+}
+
+static VALUE pl_alloc(VALUE klass) {
+  return pl_as_ruby_class( create_probability_list(), klass );
 }
 
 inline static ProbabilityList *get_probability_list( VALUE obj ) {
@@ -98,7 +129,7 @@ VALUE probabilities_for_fair_die( VALUE self, VALUE sides ) {
   VALUE obj = pl_alloc( NewProbabilities );
   ProbabilityList *pl = get_probability_list( obj );
   pl->offset = 1;
-  init_probs_iv( pl, s, 1.0/s );
+  alloc_probs_iv( pl, s, 1.0/s );
   return obj;
 }
 
@@ -113,10 +144,37 @@ VALUE probabilities_add_distributions( VALUE self, VALUE gdpa, VALUE gdpb ) {
   VALUE obj = pl_alloc( NewProbabilities );
   ProbabilityList *pl = get_probability_list( obj );
   pl->offset = o;
-  init_probs_iv( pl, s, 0.0 );
-
-  double *pr = pl->probs;
+  double *pr = alloc_probs_iv( pl, s, 0.0 );
   for ( i=0; i < pl_a->slots; i++ ) { for ( j=0; j < pl_b->slots; j++ ) {
+    pr[ i + j ] += (pl_a->probs)[i] * (pl_b->probs)[j];
+  } }
+  return obj;
+}
+
+VALUE probabilities_add_distributions_mult( VALUE self, VALUE m_a, VALUE gdpa, VALUE m_b, VALUE gdpb ) {
+  // TODO: Confirm types before progressing, factor into "native" and "bound" parts
+  int mul_a = NUM2INT( m_a );
+  ProbabilityList *pl_a = get_probability_list( gdpa );
+  int mul_b = NUM2INT( m_b );
+  ProbabilityList *pl_b = get_probability_list( gdpb );
+
+  int pts[4] = {
+    mul_a * pl_min( pl_a ) + mul_b * pl_min( pl_b ),
+    mul_a * pl_max( pl_a ) + mul_b * pl_min( pl_b ),
+    mul_a * pl_min( pl_a ) + mul_b * pl_max( pl_b ),
+    mul_a * pl_max( pl_a ) + mul_b * pl_max( pl_b ) };
+
+  int combined_min = min( pts, 4 );
+  int combined_max = max( pts, 4 );
+  int s =  1 + combined_max - combined_min;
+
+  VALUE obj = pl_alloc( NewProbabilities );
+  ProbabilityList *pl = get_probability_list( obj );
+  pl->offset = combined_min;
+  double *pr = alloc_probs_iv( pl, s, 0.0 );
+  int i,j;
+  for ( i=0; i < pl_a->slots; i++ ) { for ( j=0; j < pl_b->slots; j++ ) {
+    int k = mul_a * (i + pl_a->offset) + mul_b * (j + pl_b->offset) - combined_min;
     pr[ i + j ] += (pl_a->probs)[i] * (pl_b->probs)[j];
   } }
   return obj;
@@ -129,5 +187,6 @@ void init_probabilities_class( VALUE ParentModule ) {
   rb_define_method( NewProbabilities, "to_h", probabilities_to_h, 0 );
   rb_define_singleton_method( NewProbabilities, "for_fair_die", probabilities_for_fair_die, 1 );
   rb_define_singleton_method( NewProbabilities, "add_distributions", probabilities_add_distributions, 2 );
+  rb_define_singleton_method( NewProbabilities, "add_distributions_mult", probabilities_add_distributions_mult, 4 );
   return;
 }
--- spec/new_probability_spec.rb
+++ spec/new_probability_spec.rb
@@ -54,5 +54,31 @@ describe GamesDice::NewProbabilities do
       end
     end
 
+    describe "#add_distributions_mult" do
+      it "should combine two multiplied distributions to create a third one" do
+        d4a = GamesDice::NewProbabilities.new( [ 1.0/4, 1.0/4, 1.0/4, 1.0/4 ], 1 )
+        d4b = GamesDice::NewProbabilities.new( [ 1.0/10, 2.0/10, 3.0/10, 4.0/10], 1 )
+        p = GamesDice::NewProbabilities.add_distributions_mult( 2, d4a, -1, d4b )
+        p.to_h.should be_valid_distribution
+      end
+
+      it "should calculate a distribution for '1d6 - 1d4' accurately" do
+        d6 = GamesDice::NewProbabilities.for_fair_die( 6 )
+        d4 = GamesDice::NewProbabilities.for_fair_die( 4 )
+        p = GamesDice::NewProbabilities.add_distributions_mult( 1, d6, -1, d4 )
+        h = p.to_h
+        h.should be_valid_distribution
+        h[-3].should be_within(1e-9).of 1.0/24
+        h[-2].should be_within(1e-9).of 2.0/24
+        h[-1].should be_within(1e-9).of 3.0/24
+        h[0].should be_within(1e-9).of 4.0/24
+        h[1].should be_within(1e-9).of 4.0/24
+        h[2].should be_within(1e-9).of 4.0/24
+        h[3].should be_within(1e-9).of 3.0/24
+        h[4].should be_within(1e-9).of 2.0/24
+        h[5].should be_within(1e-9).of 1.0/24
+      end
+    end
+
   end
 end
