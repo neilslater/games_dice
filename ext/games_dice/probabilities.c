e240e8dd599aa46c2f1f81ef5ad9556976a7905c
Neil Slater <neil@aframe.com>
2013-07-03 23:42:49 +0100

    Add Ruby 1.8.7 patch for DBL2NUM

--- ext/games_dice/probabilities.c
+++ ext/games_dice/probabilities.c
@@ -1,7 +1,13 @@
 // ext/games_dice/probabilities.c
 
 #include <stdio.h>
+#include <ruby.h>
 #include "probabilities.h"
+
+// Ruby 1.8.7 compatibility patch
+#ifndef DBL2NUM
+#define DBL2NUM( dbl_val ) rb_float_new( dbl_val )
+#endif
 
 VALUE NewProbabilities = Qnil;
 
