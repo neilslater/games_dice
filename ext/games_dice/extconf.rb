8a5f2f54cccd4b6903d7872feb954379f14564bf
Neil Slater <neil@aframe.com>
2013-07-15 12:33:06 +0100

    Stop detecting Ruby types, and rely on mkmf to make the correct decision

--- ext/games_dice/extconf.rb
+++ ext/games_dice/extconf.rb
@@ -1,11 +1,25 @@
 # ext/games_dice/extconf.rb
-if RUBY_DESCRIPTION =~ /jruby/
+can_compile_extensions = false
+want_extensions = true
+
+begin
+  require 'mkmf'
+  can_compile_extensions = true
+rescue Exception
+  # This will appear only in verbose mode.
+  $stderr.puts "Could not require 'mkmf'. Not fatal: The extensions are optional."
+end
+
+
+if can_compile_extensions && want_extensions
+  create_makefile( 'games_dice/games_dice' )
+
+else
+  # Create a dummy Makefile, to satisfy Gem::Installer#install
   mfile = open("Makefile", "wb")
   mfile.puts '.PHONY: install'
   mfile.puts 'install:'
   mfile.puts "\t" + '@echo "Extensions not installed, falling back to pure Ruby version."'
   mfile.close
-else
-  require 'mkmf'
-  create_makefile( 'games_dice/games_dice' )
+
 end
