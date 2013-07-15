7712ebce28a71813c401fd27ad6578240de1b8c7
Neil Slater <neil@aframe.com>
2013-07-15 10:05:52 +0100

    Do-nothing Makefile for jruby

--- ext/games_dice/extconf.rb
+++ ext/games_dice/extconf.rb
@@ -1,3 +1,11 @@
 # ext/games_dice/extconf.rb
-require 'mkmf'
-create_makefile( 'games_dice/games_dice' )
+if RUBY_DESCRIPTION =~ /jruby/
+  mfile = open("Makefile", "wb")
+  mfile.puts '.PHONY: install'
+  mfile.puts 'install:'
+  mfile.puts "\t" + '@echo "Extensions not installed, falling back to pure Ruby version."'
+  mfile.close
+else
+  require 'mkmf'
+  create_makefile( 'games_dice/games_dice' )
+end
