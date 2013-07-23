1ad825cca7571249596eb532d611cbff9e8453a4
Neil Slater <neil@aframe.com>
2013-07-23 19:28:32 +0100

    Break recursive_probabilities up into 3 separate methods

--- lib/games_dice/complex_die.rb
+++ lib/games_dice/complex_die.rb
@@ -142,11 +142,7 @@ class GamesDice::ComplexDie
     rerolls_remaining = @rerolls.map { |rule| rule.limit }
 
     loop do
-      # Find which rule, if any, is being triggered
-      rule_idx = @rerolls.zip(rerolls_remaining).find_index do |rule,remaining|
-        next if rule.type == :reroll_subtract && @result.rolls.length > 1
-        remaining > 0 && rule.applies?( @basic_die.result )
-      end
+      rule_idx = find_matching_reroll_rule( @basic_die.result, @result.rolls.length ,rerolls_remaining )
       break unless rule_idx
 
       rule = @rerolls[ rule_idx ]
@@ -159,6 +155,14 @@ class GamesDice::ComplexDie
       else
         @result.add_roll( @basic_die.roll, rule.type )
       end
+    end
+  end
+
+  # Find which rule, if any, is being triggered
+  def find_matching_reroll_rule check_value, num_rolls, rerolls_remaining
+    @rerolls.zip(rerolls_remaining).find_index do |rule,remaining|
+      next if rule.type == :reroll_subtract && num_rolls > 1
+      remaining > 0 && rule.applies?( check_value )
     end
   end
 
@@ -251,41 +255,44 @@ class GamesDice::ComplexDie
   def recursive_probabilities probabilities={},prior_probability=1.0,depth=0,prior_result=nil,rerolls_left=nil,roll_reason=:basic,subtracting=false
     each_probability = prior_probability / @basic_die.sides
     depth += 1
-    if depth >= 20 || each_probability < 1.0e-12
+    if depth >= 20 || each_probability < 1.0e-16
       @probabilities_complete = false
       stop_recursing = true
     end
 
     @basic_die.each_value do |v|
-      # calculate value, recurse if there is a reroll
-      result_so_far, rerolls_remaining = calc_result_so_far(prior_result, rerolls_left, v, roll_reason )
-
-      # Find which rule, if any, is being triggered
-      rule_idx = @rerolls.zip(rerolls_remaining).find_index do |rule,remaining|
-        next if rule.type == :reroll_subtract && result_so_far.rolls.length > 1
-        remaining > 0 && rule.applies?( v )
-      end
-
-      if rule_idx && ! stop_recursing
-        rule = @rerolls[ rule_idx ]
-        rerolls_remaining[ rule_idx ] -= 1
-        is_subtracting = true if subtracting || rule.type == :reroll_subtract
-
-        # Apply the rule (note reversal for additions, after a subtract)
-        if subtracting && rule.type == :reroll_add
-          recursive_probabilities probabilities,each_probability,depth,result_so_far,rerolls_remaining,:reroll_subtract,is_subtracting
-        else
-          recursive_probabilities probabilities,each_probability,depth,result_so_far,rerolls_remaining,rule.type,is_subtracting
-        end
-      # just accumulate value on a regular roll
-      else
-        t = result_so_far.total
-        probabilities[ t ] ||= 0.0
-        probabilities[ t ] += each_probability
-      end
-
+      recurse_probs_for_value( v, roll_reason, probabilities, each_probability, depth, prior_result, rerolls_left, subtracting, stop_recursing )
     end
     probabilities
+  end
+
+  def recurse_probs_for_value v, roll_reason, probabilities, each_probability, depth, prior_result, rerolls_left, subtracting, stop_recursing
+    # calculate value, recurse if there is a reroll
+    result_so_far, rerolls_remaining = calc_result_so_far(prior_result, rerolls_left, v, roll_reason )
+
+    # Find which rule, if any, is being triggered
+    rule_idx = find_matching_reroll_rule( v, result_so_far.rolls.length, rerolls_remaining )
+
+    if rule_idx && ! stop_recursing
+      recurse_probs_with_rule( probabilities, each_probability, depth, result_so_far, rerolls_remaining, rule_idx, subtracting )
+    else
+      t = result_so_far.total
+      probabilities[ t ] ||= 0.0
+      probabilities[ t ] += each_probability
+    end
+  end
+
+  def recurse_probs_with_rule probabilities, each_probability, depth, result_so_far, rerolls_remaining, rule_idx, subtracting
+    rule = @rerolls[ rule_idx ]
+    rerolls_remaining[ rule_idx ] -= 1
+    is_subtracting = true if subtracting || rule.type == :reroll_subtract
+
+    # Apply the rule (note reversal for additions, after a subtract)
+    if subtracting && rule.type == :reroll_add
+      recursive_probabilities probabilities, each_probability, depth, result_so_far, rerolls_remaining, :reroll_subtract, is_subtracting
+    else
+      recursive_probabilities probabilities, each_probability, depth, result_so_far, rerolls_remaining, rule.type, is_subtracting
+    end
   end
 
   def calc_result_so_far prior_result, rerolls_left, v, roll_reason
