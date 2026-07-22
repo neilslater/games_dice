6ea37ca54435857b2c86dd65082ce5309b95cd82
Neil Slater <slobo777@gmail.com>
2026-07-22 21:24:19 +0100

    Add native extension quality checks

--- ext/games_dice/extconf.rb
+++ ext/games_dice/extconf.rb
@@ -3,5 +3,34 @@
 # ext/games_dice/extconf.rb
 
 require 'mkmf'
+require 'rbconfig'
+
+# mkmf exposes compiler and linker flags through these globals.
+# rubocop:disable Style/GlobalVars
+case ENV.fetch('GAMES_DICE_NATIVE_MODE', 'release')
+when 'release'
+  # Use Ruby's normal extension build flags.
+when 'lint'
+  $CFLAGS << ' -std=gnu2x -O0 -g'
+  $CFLAGS << ' -Wall -Wextra -Wpedantic -Wformat=2 -Werror'
+
+  cc = RbConfig::CONFIG.fetch('CC')
+  host_os = RbConfig::CONFIG.fetch('host_os')
+  if cc.include?('clang') || host_os.include?('darwin')
+    # Ruby callback signatures legitimately leave some parameters unused, and
+    # Ruby 4 headers use standard attributes that Clang treats as C23 syntax.
+    $CFLAGS << ' -Wno-strict-prototypes -Wno-unused-parameter -Wno-c23-extensions'
+  end
+when 'coverage'
+  $CFLAGS << ' -O0 -g --coverage'
+  $LDFLAGS << ' --coverage'
+when 'sanitize'
+  $CFLAGS << ' -O1 -g -fsanitize=address,undefined'
+  $CFLAGS << ' -fno-omit-frame-pointer'
+  $LDFLAGS << ' -fsanitize=address,undefined'
+else
+  abort "Unknown GAMES_DICE_NATIVE_MODE: #{ENV.fetch('GAMES_DICE_NATIVE_MODE', nil)}"
+end
+# rubocop:enable Style/GlobalVars
 
 create_makefile('games_dice/games_dice')
--- ext/games_dice/probabilities.h
+++ ext/games_dice/probabilities.h
@@ -7,7 +7,7 @@
 
 #include <ruby.h>
 
-void init_probabilities_class();
+void init_probabilities_class(void);
 
 typedef struct _pd {
     int offset;
@@ -16,25 +16,9 @@ typedef struct _pd {
     double *cumulative;
   } ProbabilityList;
 
-inline int pl_min( ProbabilityList *pl );
-
-inline int pl_max( ProbabilityList *pl );
-
 ProbabilityList *pl_add_distributions( ProbabilityList *pl_a, ProbabilityList *pl_b );
 
 ProbabilityList *pl_add_distributions_mult( int mul_a, ProbabilityList *pl_a, int mul_b, ProbabilityList *pl_b );
-
-inline double pl_p_eql( ProbabilityList *pl, int target );
-
-inline double pl_p_gt( ProbabilityList *pl, int target );
-
-inline double pl_p_lt( ProbabilityList *pl, int target );
-
-inline double pl_p_le( ProbabilityList *pl, int target );
-
-inline double pl_p_ge( ProbabilityList *pl, int target );
-
-inline double pl_expected( ProbabilityList *pl );
 
 ProbabilityList *pl_given_ge( ProbabilityList *pl, int target );
 
